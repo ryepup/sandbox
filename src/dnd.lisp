cc98950c948516425dfea8aff02ec0ad9531ff8c
Ryan Davis <ryan@mokeys.org>
2009-06-22 23:05:23 -0400

    Silly little program to easily record dice rolls for dnd stats keeping.
    It has a simple loop asking for data in this format:
    player dice-type roll+ [x dice-type roll+]+
    
    player - 2 letter string representing player (could be longer, but 2 is enough)
    dice-type - 20, 12, 10, 8, 6, or 4
    roll - the number actually rolled
    x - any letter

---
+++ src/dnd.lisp
@@ -0,0 +1,37 @@
+(require 'cl-ppcre)
+(require 'alexandria)
+(require 'split-sequence)
+(require 'iterate)
+(use-package 'iterate)
+(defvar *rolls* (make-hash-table))
+
+(defun read-rolls ()
+  (iter
+   (print-rolls T 0 (save-roll (read-line)))))
+
+(defun save-roll (rolldef)
+  (labels
+      ((ensure-subhash (key hash)
+	 (alexandria:ensure-gethash key hash (make-hash-table)))
+       (parse-rolls (hash s)
+	 (handler-case
+	     (iter (with dice = (ensure-subhash (read s) hash))
+		   (for roll = (read s))
+		   (until (not (numberp roll)))		  
+		   (incf (gethash roll dice 0))
+		   (finally (return T)))
+	   (end-of-file () nil))))
+    (with-input-from-string (s rolldef)
+      (let ((player-rolls (ensure-subhash (read s) *rolls* )))
+	(iter (while (parse-rolls player-rolls s)))
+	player-rolls))))
+
+
+(defun print-rolls (&optional (stream T) (indent 0) (hash *rolls*))
+  (iter (for (k v) in-hashtable hash)
+	(with indentation = (make-array indent :element-type 'character :initial-element #\space))	
+	(if (hash-table-p v)
+	    (progn
+	      (format stream "~a~a:~%" indentation k)
+	      (print-hash stream (+ 1 indent) v))
+	    (format stream "~a~a:~a~%" indentation k v))))
